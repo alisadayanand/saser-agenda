4c24b3afc3f24d7da6d7820812a3ca774344fa6f
rupnarainP <prdaxter@gmail.com>
2021-12-09 01:38:07 +0200

    Updated the front-end logic and added additional rest calls.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+import sys
+
+from flask import Flask, render_template, redirect, request, session, url_for, jsonify
+import mariadb
+
 
 main = Flask(__name__)
 
@@ -17,6 +21,64 @@ def edit():
 
 @main.route('/view', methods=['POST', 'GET'])
 def view():
-    return render_template('agenda_view.html')
+    return render_template('agenda_view.html', test="fdsfsdfsf")
+
+
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="root",  # Username
+        password="root",  # Password
+        host="127.0.0.1",  # Host IP
+        port=3306,
+        database="sgtdevsaser"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+@main.route('/getAgenda', methods=['POST', 'GET'])
+def getAgenda():
+
+    result = []
+    script = """SELECT * FROM sgtdevsaser.agenda"""
+    cs = conn.cursor()
+    cs.execute(script)
+    for row in cs:
+        result.append(row)
+
+    print(result)
+    return str(result)
+
+
+@main.route('/viewAgendaByDate', methods=['POST', 'GET'])
+def viewAgendaByDate():
+
+    date = request.form['date']
+    print(date)
+
+    result = []
+    script = """SELECT * FROM sgtdevsaser.agenda WHERE date = '{}'""".format(date)
+    cs = conn.cursor()
+    cs.execute(script)
+    for row in cs:
+        result.append(row)
+
+    print(result)
+    # return str(result)
+
+    # [(1, 1, datetime.date(2021, 12, 10), datetime.timedelta(seconds=39600), datetime.timedelta(seconds=46800),
+    #   'Microsoft Teams', 'Craig Cadenhead', 'Courtley Whittaker (SGT)', 'Edwin Jacobs', 'Theo Mabaso', 'Helen Meyer',
+    #   datetime.date(2021, 12, 15))]
+
+    # for column in result:
+    #
+    # response = {'meetingName': 'SGT SASER',
+    #             'location': }
+    # return
+    return render_template('agenda_view.html', location="Microsoft Teams")
+
 
 main.run(debug=True)
